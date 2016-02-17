bd4ad4d70bebaf332d096b94c86d3b8f758c925e
Shohei Aoki <shoaok@gmail.com>
2016-02-17 12:46:41 +0900

    initial commit

---
+++ moge.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+HOMEBREW_BREWALL_VERSION = '0.0.1'
+class Brewall < Formula
+  homepage 'https://github.com/shohei/homebrew-moge/'
+  url 'https://github.com/shohei/homebrew-moge.git', :tag => "v#{HOMEBREW_BREWALL_VERSION}"
+  version HOMEBREW_BREWALL_VERSION
+  head 'https://github.com/shohei/homebrew-moge.git', :branch => 'master'
+
+  skip_clean 'bin'
+
+  def install
+    prefix.install 'bin'
+    (bin+'moge').chmod 0755
+  end
+end
