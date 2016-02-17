cd899e2d7b32dd866f0fdc26cf52e02f839fb210
Shohei Aoki <shoaok@gmail.com>
2016-02-17 13:20:26 +0900

    change repo

--- mogebar.rb
+++ mogebar.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 HOMEBREW_MOGEBAR_VERSION = '0.0.1'
 class Mogebar < Formula
-  homepage 'https://github.com/shohei/homebrew-mogebar/'
+  # homepage 'https://github.com/shohei/homebrew-mogebar/'
+  homepage 'https://github.com/shohei/homebrew-mogebar/archive/master.zip'
   url 'https://github.com/shohei/homebrew-mogebar.git', :tag => "v#{HOMEBREW_MOGEBAR_VERSION}"
   version HOMEBREW_MOGEBAR_VERSION
   head 'https://github.com/shohei/homebrew-mogebar.git', :branch => 'master'
 
-  skip_clean 'bin/mogebar'
+  skip_clean 'bin'
 
   def install
     dir = `pwd`
