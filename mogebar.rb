81f210188ba29ef93bf4888e6b533e090b06305d
Shohei Aoki <shoaok@gmail.com>
2016-02-17 13:29:16 +0900

    debug

--- mogebar.rb
+++ mogebar.rb
@@ -1,33 +1,16 @@
 require 'formula'
 
-HOMEBREW_MOGEBAR_VERSION = '0.0.3'
+HOMEBREW_MOGEBAR_VERSION = '0.0.4'
 class Mogebar < Formula
-  # homepage 'https://github.com/shohei/homebrew-mogebar/'
-  homepage 'https://github.com/shohei/homebrew-mogebar/archive/master.zip'
+  homepage 'https://github.com/shohei/homebrew-mogebar/'
   url 'https://github.com/shohei/homebrew-mogebar.git', :tag => "v#{HOMEBREW_MOGEBAR_VERSION}"
   version HOMEBREW_MOGEBAR_VERSION
   head 'https://github.com/shohei/homebrew-mogebar.git', :branch => 'master'
 
-  dir = `pwd`
-  puts dir
-  puts 'initial'
-  files= `ls`
-  puts files 
   skip_clean 'bin'
-  puts 'after skip_clean'
-  files= `ls`
-  puts files 
 
   def install
-  puts 'inside install'
-    files= `ls`
-    puts files 
-    com = `cat moge.rb`
-    puts com
-    dir = `pwd`
-    puts dir
-    puts 'mogemoge'
-    bin.install 'bin/mogebar'
-    # (bin+'mogebar').chmod 0755
+    prefix.install 'bin'
+    (bin+'mogebar').chmod 0755
   end
 end
